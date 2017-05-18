2a509a37347b203cd6910243156d139c087ed1c1
jen8 <asianstrategy2@gmail.com>
2017-05-17 23:42:55 -0700

    Update quiz2.js

--- quiz2.js
+++ quiz2.js
@@ -1,7 +1,7 @@
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Our app is running on port ${ PORT }`);
-});
+//const PORT = process.env.PORT || 3000;
+//app.listen(PORT, () => {
+//    console.log(`Our app is running on port ${ PORT }`);
+//});
 
 
 var question = [
